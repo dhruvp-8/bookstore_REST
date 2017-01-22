474b2db7a57157c43e6d0d44607485347f23e39a
dhruvp-8 <dhruvpatel5738@gmail.com>
2017-01-23 01:32:38 +0530

    Done!

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/client'));
 app.use(bodyParser.json());
 
@@ -122,5 +123,6 @@ app.get('/api/books/:_id',function(req,res){
     })
 });
 
-app.listen(5000);
-console.log('Running on port 5000...');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
